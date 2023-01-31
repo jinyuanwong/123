0393b80dbd0bc31d81cb93711dd60a52668b902b
Jinyuan <jinyuanwang99@gmail.com>
2023-01-31 16:29:50 +0800

    routed.rb updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'static_pages#home'
+  get 'static_pages/help'
+  get 'static_pages/about'
   # Defines the root path route ("/")
   # root "articles#index"
 end
